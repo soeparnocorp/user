61c7afc934c8f8112e6624a33c3800c5bf24d6ea
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-01-22 02:38:58 +0700

    $id.tsx

--- app/routes/$id.tsx
+++ app/routes/$id.tsx
@@ -69,7 +69,7 @@ export default function () {
 						type="submit"
 						name="intent"
 						value="create"
-						className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition"
+						className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition"
 					>
 						Save
 					</button>
